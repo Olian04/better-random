bfdd5e0fb888572633f90f7783f02cff48dea825
Oliver Anteros <rift95@live.se>
2019-09-30 00:52:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const between = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
+const procentage = () => between(0, 100) / 100;
 
 Math.random.between = between;
-Math.random.rgba = () => `rgba(${between(0, 255)}, ${between(0, 255)}, ${between(0, 255)}, ${between(0, 100) / 100})`;
-Math.random.rgb = () => `rgba(${between(0, 255)}, ${between(0, 255)}, ${between(0, 255)})`;
+Math.random.procentage = procentage;
 Math.random.select = (array) => array[between(0, array.length-1)];
+Math.random.rgba = () => `rgba(${between(0, 255)}, ${between(0, 255)}, ${between(0, 255)}, ${procentage()})`;
+Math.random.rgb = () => `rgba(${between(0, 255)}, ${between(0, 255)}, ${between(0, 255)})`;
 Math.random.pixel = () => {
-  const canvas = document.createElement('canvas');
+	const canvas = document.createElement('canvas');
   canvas.height = canvas.width = 1;
   const ctx = canvas.getContext('2d');
   ctx.fillStyle = Math.random.rgb();
