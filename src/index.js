d16506384d102a7ceb70f305ba8014a59a2a3988
Oliver Anteros <rift95@live.se>
2019-09-30 01:04:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ Math.random.select = (array) => array[between(0, array.length-1)];
 Math.random.rgba = () => `rgba(${between(0, 255)}, ${between(0, 255)}, ${between(0, 255)}, ${procentage()})`;
 Math.random.rgb = () => `rgba(${between(0, 255)}, ${between(0, 255)}, ${between(0, 255)})`;
 Math.random.pixel = () => {
-	const canvas = document.createElement('canvas');
+  const canvas = document.createElement('canvas');
   canvas.height = canvas.width = 1;
   const ctx = canvas.getContext('2d');
   ctx.fillStyle = Math.random.rgb();
