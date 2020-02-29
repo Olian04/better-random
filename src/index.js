71773c1cfd3c4bb4c0024ecdc01671edac699f17
Oliver Anteros <rift95@live.se>
2020-02-29 22:42:25 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const between = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
-const percentages = () => between(0, 100) / 100;
+const percentage = () => between(0, 100) / 100;
 
 Math.random.between = between;
-Math.random.percentages = percentages;
+Math.random.percentage = percentage;
 Math.random.select = (array) => array[between(0, array.length-1)];
-Math.random.rgba = () => `rgba(${between(0, 255)}, ${between(0, 255)}, ${between(0, 255)}, ${percentages()})`;
+Math.random.rgba = () => `rgba(${between(0, 255)}, ${between(0, 255)}, ${between(0, 255)}, ${percentage()})`;
 Math.random.rgb = () => `rgba(${between(0, 255)}, ${between(0, 255)}, ${between(0, 255)})`;
 Math.random.pixel = () => {
   const canvas = document.createElement('canvas');
